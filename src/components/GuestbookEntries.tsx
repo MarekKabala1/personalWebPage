ee9697eb535a116569110a52912e258a735ae653
MarekKabala1 <marekkabala1@gmail.com>
2025-04-26 18:32:16 +0100

    feat:add guest form

---
+++ src/components/GuestbookEntries.tsx
@@ -0,0 +1,29 @@
+// GuestbookEntries.jsx
+import { useQuery } from 'convex/react';
+import { api } from '../../convex/_generated/api';
+import { withConvexProvider } from '../lib/convex';
+
+export function GuestbookEntries() {
+  const entries = useQuery(api.guestbook.get) || [];
+  console.log(entries);
+
+  return (
+    <div>
+      {entries.map((entry) => (
+        <div
+          key={entry._id}
+          className='retro-window'
+          style={{ marginTop: '10px' }}
+        >
+          <div className='retro-window-title guestbook-title'>
+            <p>{entry.name}</p>
+            <p>{new Date(entry._creationTime).toLocaleDateString()}</p>
+          </div>
+          <p style={{ padding: '10px' }}>{entry.message}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default withConvexProvider(GuestbookEntries);
